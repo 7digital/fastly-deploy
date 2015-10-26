36fea95ab6dc558977dd012bcf94b6291f2b6a97
jenny r <jenny.ramsay@7digital.com>
2015-10-26 11:29:37 +0000

    [EA/JR] - Add test to ensure clones active version not latest version

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -21,9 +21,7 @@ RSpec.describe "fastly-deploy" do
                           name: "DeployTestBackend",
                           ipv4: "192.0.43.10",
                           port: 80)
-    vcl_contents = File.read("spec/test.vcl")
-    @version.upload_vcl "Main", vcl_contents
-    @version.vcl("Main").set_main!
+    upload_vcl_to_version(@version, 'spec/test.vcl')
     @version.activate!
 
     puts "Activated service. Running test."
@@ -36,6 +34,20 @@ RSpec.describe "fastly-deploy" do
 
       deploy_vcl @api_key, @service.id, "spec/test.vcl", false
 
+    end
+
+    it 'gets the active version not the latest version' do
+      non_active_version = create_non_active_version_with_another_domain
+
+      expect(number_of_domains_for_version(@version)).to eq(1)
+      expect(number_of_domains_for_version(non_active_version)).to eq(2)
+      
+
+      deploy_vcl @api_key, @service.id, "spec/test.vcl", false
+
+      new_active_version = get_active_version()
+      expect(new_active_version.number).not_to eq(@version.number)
+      expect(number_of_domains_for_version(new_active_version)).to eq(1)
     end
 
   end
@@ -53,4 +65,29 @@ RSpec.describe "fastly-deploy" do
   end
 
 end
+
+def upload_vcl_to_version(version, file_path) 
+  vcl_contents = File.read(file_path)
+  version.upload_vcl "Main", vcl_contents
+  version.vcl("Main").set_main!
+end  
+
+def create_non_active_version_with_another_domain
+  non_active_version = @version.clone
+  random_suffix = ('a'..'z').to_a.shuffle[0,8].join
+  @fastly.create_domain(service_id: @service.id,
+                        version: non_active_version.number,
+                        name: "deploytestservice2-#{random_suffix}.com")
+  return non_active_version
+end 
+
+def number_of_domains_for_version(version)
+  return @fastly.list_domains(service_id: @service.id, version: version.number).length
+end
+
+def get_active_version
+  service = @fastly.get_service(@service.id)
+  active_version = service.versions.find{|ver| ver.active?}
+  return active_version
+end  
 
