a5ea25a3749f6fae7b568698641f33eab903d792
jenny r <jenny.ramsay@7digital.com>
2015-11-04 16:56:28 +0000

    [JR] Add first level deep search for includes

--- lib/detect_includes.rb
+++ lib/detect_includes.rb
@@ -1,4 +1,10 @@
-def get_includes(main_vcl_path)
-  return File.readlines(main_vcl_path).select { |line| /^include "(.*)"$/.match(line) }
+def get_includes(main_vcl_path, includes_dir)
+  main_includes = get_includes_for_vcl main_vcl_path, includes_dir
+  return main_includes.concat main_includes.map{| include_vcl | get_includes_for_vcl include_vcl, includes_dir}.flatten
+end
+
+def get_includes_for_vcl(vcl_path, includes_dir) 
+  return File.readlines(vcl_path).select { |line| /^include "(.*)"$/.match(line) }
             .map{ |line| /^include "(.*)"$/.match(line)[1] }
-end  
+            .map{ |vcl_file_name| File.join(includes_dir, vcl_file_name + ".vcl")}
+end
--- spec/detect_includes_spec.rb
+++ spec/detect_includes_spec.rb
@@ -4,20 +4,27 @@ require_relative '../lib/detect_includes.rb'
 RSpec.describe "get_includes" do
 
   it "should return empty array if there are no includes" do
-    expect(get_includes("spec/vcls/detect_includes_vcls/no_includes.vcl")).to be_empty
+    expect(get_includes("spec/vcls/detect_includes_vcls/no_includes.vcl", "spec/vcls/includes")).to be_empty
   end
 
   it "should return the include if there is one include at top of vcl" do
-    includes = get_includes("spec/vcls/detect_includes_vcls/one_include_at_top.vcl")
+    includes = get_includes("spec/vcls/detect_includes_vcls/one_include_at_top.vcl", "spec/vcls/includes")
     expect(includes.length).to eq(1)
-    expect(includes).to include("new_test_include")
+    expect(includes).to include("spec/vcls/includes/new_test_include.vcl")
   end
 
   it "should return all includes" do
-    includes = get_includes("spec/vcls/detect_includes_vcls/multiple_includes.vcl")
+    includes = get_includes("spec/vcls/detect_includes_vcls/multiple_includes.vcl", "spec/vcls/includes")
     expect(includes.length).to eq(3)
-    expect(includes).to include("new_test_include")
-    expect(includes).to include("test_include")
-    expect(includes).to include("not_uploaded_again_include")
+    expect(includes).to include("spec/vcls/includes/new_test_include.vcl")
+    expect(includes).to include("spec/vcls/includes/test_include.vcl")
+    expect(includes).to include("spec/vcls/includes/not_uploaded_again_include.vcl")
+  end
+
+  it "should look for includes in includes" do
+    includes = get_includes("spec/vcls/detect_includes_vcls/include_with_include.vcl", "spec/vcls/includes")
+    expect(includes.length).to eq(2)
+    expect(includes).to include("spec/vcls/includes/has_include.vcl")
+    expect(includes).to include("spec/vcls/includes/test_include.vcl")
   end
 end  
